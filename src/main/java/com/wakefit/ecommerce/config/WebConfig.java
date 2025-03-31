c9e21585af1b9a6739936636e0c9e3804fd323c0
Kunal Kadam <kunalkadam2762001@gmail.com>
2025-03-31 16:26:55 +0530

    Updated minor things

--- src/main/java/com/wakefit/ecommerce/config/WebConfig.java
+++ src/main/java/com/wakefit/ecommerce/config/WebConfig.java
@@ -10,20 +10,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/v1/**")
-                        .allowedOrigins(
-                                "https://mywakefit-kunal-kadams-projects.vercel.app",
-                                "https://mywakefit.vercel.app"
-                        )
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/**")
+                .allowedOrigins(
+                        "https://mywakefit-kunal-kadams-projects.vercel.app",
+                        "https://mywakefit.vercel.app"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
+
+
