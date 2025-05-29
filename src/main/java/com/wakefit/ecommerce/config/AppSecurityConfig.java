40b06e6a88eb29e45721c9774c2f8ba046ea87dd
Kunal Kadam <kunalkadam2762001@gmail.com>
2025-05-29 11:51:58 +0530

    Fix login hhtp basics

--- src/main/java/com/wakefit/ecommerce/config/AppSecurityConfig.java
+++ src/main/java/com/wakefit/ecommerce/config/AppSecurityConfig.java
@@ -67,9 +67,42 @@ public class AppSecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/v1/cart/**").hasAuthority("customer")
                         .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults())
-                .logout(logout -> logout.disable()); // Disable logout since we use stateless auth
+                .logout(logout -> logout.disable());
 
         return http.build();
     }
+
 }
+
+
+
+//@Bean
+//public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//    http
+//            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//            .cors(cors -> cors.configurationSource(request -> new org.springframework.web.cors.CorsConfiguration().applyPermitDefaultValues()))
+//            .csrf(csrf -> csrf.disable())
+//            .authorizeHttpRequests(auth -> auth
+//                    .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // CORS Preflight
+//                    .requestMatchers(HttpMethod.POST, "/api/v1/login", "/api/v1/users/login", "/api/v1/users/register").permitAll()
+//                    .requestMatchers(HttpMethod.GET, "/api/v1/users/**").hasAnyAuthority("Admin", "customer")
+//                    .requestMatchers(HttpMethod.PUT, "/api/v1/users/**").hasAnyAuthority("Admin", "customer")
+//                    .requestMatchers(HttpMethod.POST, "/api/v1/users/**").hasAuthority("Admin")
+//                    .requestMatchers(HttpMethod.POST, "/api/v1/products/**").hasAuthority("Admin")
+//                    .requestMatchers(HttpMethod.GET, "/api/v1/category/**").hasAnyAuthority("Admin", "customer")
+//                    .requestMatchers(HttpMethod.GET, "/api/v1/products/**").hasAnyAuthority("Admin", "customer")
+//                    .requestMatchers(HttpMethod.POST, "/api/v1/category/**").hasAuthority("Admin")
+//                    .requestMatchers(HttpMethod.POST, "/api/v1/feedback/**").hasAnyAuthority("Admin", "customer")
+//                    .requestMatchers(HttpMethod.GET, "/api/v1/cart/**").hasAnyAuthority("Admin", "customer")
+//                    .requestMatchers(HttpMethod.POST, "/api/v1/address/**").hasAnyAuthority("Admin", "customer")
+//                    .requestMatchers(HttpMethod.GET, "/api/v1/address/**").hasAnyAuthority("Admin", "customer")
+//                    .requestMatchers(HttpMethod.POST, "/api/v1/order/**").hasAnyAuthority("Admin", "customer")
+//                    .requestMatchers(HttpMethod.GET, "/api/v1/order/**").hasAnyAuthority("Admin", "customer")
+//                    .requestMatchers(HttpMethod.POST, "/api/v1/cart/**").hasAuthority("customer")
+//                    .anyRequest().authenticated()
+//            )
+//            .httpBasic(Customizer.withDefaults())
+//            .logout(logout -> logout.disable()); // Disable logout since we use stateless auth
+//
+//    return http.build();
+//}
